b306305b75fd58c4d8f2f2c2f592f2aa15ac830a
Xemnias <xemnias@hotmail.fr>
2012-08-22 19:19:18 +0200

    Commit 13
    Ajout d'une animation.

--- src/net/xemnias/client/EntityPlayer.java
+++ src/net/xemnias/client/EntityPlayer.java
@@ -10,7 +10,19 @@ public class EntityPlayer extends Entity
 	{
 		super(i, name, att);
 		org.newdawn.slick.SpriteSheet sheet = new SpriteSheet(CommunityGame.loader.getImageByName("playerAnimation.png"), 32, 64);
-		animation = new Animation(sheet, 200);
+		/*
+		Image[] img = new Image[8];
+		
+		img[0] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(0, 0, 32, 64);
+		img[1] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(32, 0, 32, 64);
+		img[2] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(64, 0, 46, 64);
+		img[3] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(110, 0, 40, 64);
+		img[4] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(150, 0, 32, 64);
+		img[5] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(182, 0, 32, 64);
+		img[6] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(214, 0, 38, 64);
+		img[7] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(252, 0, 44, 64);
+		*/
+		animation = new Animation(sheet, 120);
 	}
 
 
