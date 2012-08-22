7529b628bc6bfa74ae3380c41ae4ae06c039f6ba
Xemnias <xemnias@hotmail.fr>
2012-08-22 20:16:18 +0200

    Commit 14.3
    Réglage de bug causer par le ménage (la commit 14.2).

--- src/net/xemnias/client/AnimationList.java
+++ src/net/xemnias/client/AnimationList.java
@@ -19,14 +19,14 @@ public class AnimationList
 	{
 		Image[] img = new Image[8];
 		
-		img[0] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(0, 0, 32, 64);
-		img[1] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(32, 0, 32, 64);
-		img[2] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(64, 0, 46, 64);
-		img[3] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(110, 0, 40, 64);
-		img[4] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(150, 0, 32, 64);
-		img[5] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(182, 0, 32, 64);
-		img[6] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(214, 0, 38, 64);
-		img[7] = CommunityGame.loader.getImageByName("playerAnimationRun.png").getSubImage(252, 0, 44, 64);
+		img[0] = CommunityGame.loader.getAnimationByName("playerRunningRight.png").getSubImage(0, 0, 32, 64);
+		img[1] = CommunityGame.loader.getAnimationByName("playerRunningRight.png").getSubImage(32, 0, 32, 64);
+		img[2] = CommunityGame.loader.getAnimationByName("playerRunningRight.png").getSubImage(64, 0, 46, 64);
+		img[3] = CommunityGame.loader.getAnimationByName("playerRunningRight.png").getSubImage(110, 0, 40, 64);
+		img[4] = CommunityGame.loader.getAnimationByName("playerRunningRight.png").getSubImage(150, 0, 32, 64);
+		img[5] = CommunityGame.loader.getAnimationByName("playerRunningRight.png").getSubImage(182, 0, 32, 64);
+		img[6] = CommunityGame.loader.getAnimationByName("playerRunningRight.png").getSubImage(214, 0, 38, 64);
+		img[7] = CommunityGame.loader.getAnimationByName("playerRunningRight.png").getSubImage(252, 0, 44, 64);
 		
 		return img;
 	}
